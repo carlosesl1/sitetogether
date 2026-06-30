6ec0bf04ef00cdf9f80bf68776e6863134901b6f
carlosesl1 <carlosedusl10@gmail.com>
2026-06-30 00:21:30 -0300

    Place Leadster script in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,9 +49,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <Script id="leadster-neurolead" strategy="beforeInteractive">
-        {`(function(a,b,c,d){try{var e=b.head||b.getElementsByTagName("head")[0];var f=b.createElement("script");f.setAttribute("src",c);f.setAttribute("charset","UTF-8");f.defer=true;a.neuroleadId=d;e.appendChild(f)}catch(g){}})(window,document,"https://cdn.leadster.com.br/neurolead/neurolead.min.js","SdTbxs4BtXBoE0Bl0XDU5cUcz")`}
-      </Script>
+      <head>
+        <Script id="leadster-neurolead" strategy="beforeInteractive">
+          {`(function(a,b,c,d){try{var e=b.head||b.getElementsByTagName("head")[0];var f=b.createElement("script");f.setAttribute("src",c);f.setAttribute("charset","UTF-8");f.defer=true;a.neuroleadId=d;e.appendChild(f)}catch(g){}})(window,document,"https://cdn.leadster.com.br/neurolead/neurolead.min.js","SdTbxs4BtXBoE0Bl0XDU5cUcz")`}
+        </Script>
+      </head>
       <body
         className={`${jakarta.variable} antialiased`}
       >
